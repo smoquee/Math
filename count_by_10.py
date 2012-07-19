605d383f02a72d294efd2c5f9f5a9964e3db329b
Andy Peckys <apeckys@drw.com>
2012-07-19 14:06:21 -0500

    [ap,lp] Initial code push

---
+++ count_by_10.py
@@ -0,0 +1,16 @@
+__author__ = 'apeckys, lpeckys'
+
+
+def __main():
+    number = 0
+
+    while number <= 1000000:
+        print number
+        number += 5
+
+
+
+
+# This is the conventional entry point for the start of the main script
+if __name__ == '__main__':
+    __main()
